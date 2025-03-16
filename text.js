81500ef840e3617f7afecfe19badf2ded044d5d3
Nasir Ahmed <143830706+nasircste@users.noreply.github.com>
2025-03-16 15:25:44 +0600

    text.js

--- text.js
+++ text.js
@@ -1,7 +1,7 @@
 const clock=document.getElementById('clock');
 
 
-
+// setInterval(function(){},time ms a);
 setInterval(function(){
     let date=new Date();
     //console.log(date.toLocaleTimeString());
